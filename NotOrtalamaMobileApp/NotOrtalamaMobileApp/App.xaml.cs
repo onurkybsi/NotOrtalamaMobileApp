20245c93ef554dbb1ed943c6e125b12913717f2b
onurkybsi <onurbpm@outlook.com>
2020-02-09 19:04:47 +0300

    Save1

---
+++ NotOrtalamaMobileApp/NotOrtalamaMobileApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NotOrtalamaMobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.LightGray,
+                BarTextColor = Color.Black
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
